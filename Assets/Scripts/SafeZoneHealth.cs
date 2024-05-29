ccbd3badacb95dbd8e78bfd7fc55734fdf8581d5
DiegoPerdomo0805 <100246718+DiegoPerdomo0805@users.noreply.github.com>
2024-05-28 19:51:27 -0600

    Condición de victoria y derrota
    Se implementó la condición de victoria y derrota utilizando un control de colisión y salud con el área segura. Pendiente agregar una condición de contacto prolongado adicional a la colisión.

--- Assets/Scripts/SafeZoneHealth.cs
+++ Assets/Scripts/SafeZoneHealth.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class SafeZoneHealth : MonoBehaviour
 {
     public float Health;
+    public float MaxHealth = 60;
+    public float Timer = 120f;
+    public bool alive = true;
+    public bool EndGame = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Health = 60;        
+        Health = MaxHealth;      
     }
     void TakeDamage(float amount)
     {
         Health -= amount;
-        if (Health < 0)
+        if (Health <= 0)
         {
-            Health = 0;
+            alive = false;
         }
     }
 
@@ -31,6 +35,33 @@ public class SafeZoneHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!EndGame)
+        {
+            if (!alive) EndGame = true;
+            else
+            {
+                Timer -= Time.deltaTime;
+                if (Timer <= 0)
+                {
+                    Timer = 0;
+                    EndGame = true;
+                }
+            }
+        }
+        else
+        {
+            if(alive) YouWin();
+            else YouLose();
+        }
+    }
+
+    void YouWin()
+    {
+        Debug.Log(" - You win!");
+    }
+
+    void YouLose()
+    {
+        Debug.Log(" - You Lose! ");
     }
 }
